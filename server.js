2e92e17f72174f7db70a022923750185ffe436a0
Max Fontani <greengotty@gmail.com>
2021-02-21 17:06:33 +0200

    fixing cors

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ import handleSignin from './controllers/signin.js'
 import handleProfile from './controllers/profile.js'
 import image from './controllers/image.js'
 
-
 const app = express()
 const db = knex({
     client: 'pg',
@@ -20,7 +19,14 @@ const db = knex({
     }
 })
 
-app.use(cors())
+const corsOptions = {
+    origin: '*',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(favicon(path.join(process.env.PWD, '/favicon.ico')))
