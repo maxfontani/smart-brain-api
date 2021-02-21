efaee6fa9ad26519c622309bca8bef5abfb58556
Max Fontani <greengotty@gmail.com>
2021-02-21 11:11:46 +0200

    adding port

--- server.js
+++ server.js
@@ -32,4 +32,4 @@ app.put('/image', (req,res) => image.handleImage(req,res,db))
 app.post('/imageurl', (req,res) => image.handleApiCall(req,res))
 
 
-app.listen(4000, () => console.log('App is running on port 4000'))
+app.listen(process.env.PORT || 4000, () => console.log(`App is running on port ${PORT}`))
