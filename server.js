6560c32ca8dd1b30ecc50accc8c2c360686f3ae7
Max Fontani <greengotty@gmail.com>
2021-02-21 17:18:12 +0200

    testing

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = knex({
 })
 
 const corsOptions = {
-    origin: '*',
+    origin: 'http://localhost:3000/',
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
